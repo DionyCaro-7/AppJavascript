6561666ddec16644bf5f82a10898ad4963527008
DionyCaro-7 <dionicaro007@gmail.com>
2022-10-14 22:53:21 -0500

    Clase03 completada

--- 1821-manipulando-dom-con-js-proyecto_inicial/script.js
+++ 1821-manipulando-dom-con-js-proyecto_inicial/script.js
@@ -1,9 +1,46 @@
+
+(() => {
 const btn = document.querySelector("[data-form-btn]");
 
 const createTask = (e) =>{
     e.preventDefault();
     const input = document.querySelector("[data-form-input]");
-    console.log(input.value);
+    const value = input.value;
+    const list = document.querySelector("[data-list]");
+    const task = document.createElement("li");
+    task.classList.add("card");
+    input.value = "";
+    const taskContent = document.createElement("div");
+    taskContent.appendChild(checkComplete());
+    const titleTask = document.createElement("apan");
+    titleTask.classList.add("task");
+    titleTask.innerText = value;
+    taskContent.appendChild(titleTask);
+    const content = `
+                        
+                    <i class="fas fa-trash-alt trashIcon icon"></i>`
+    
+    //task.innerHTML = content;  
+    task.appendChild(taskContent);
+    list.appendChild(task);              
+    
 }
 
 btn.addEventListener("click", createTask);
+const checkComplete = () => {
+    const i = document.createElement("i");
+    i.classList.add("far","fa-check-square","icon");
+    i.addEventListener("click", completeTask);
+    return i;
+};
+
+//Immediately invoked function expression IIFE
+const completeTask = (event) =>{
+    const element = event.target
+    element.classList.toggle("fas");
+    element.classList.toggle("completeIcon");
+    element.classList.toggle("far");
+    
+}
+
+})();
